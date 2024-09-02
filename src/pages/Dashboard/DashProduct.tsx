36a1047f84e52fde72075ee42bd9151bc70555d5
summereuna <summereuna@gmail.com>
2024-09-02 10:56:06 +0900

    [FEAT]:상품 관리 페이지 #29

--- src/pages/Dashboard/DashProduct.tsx
+++ src/pages/Dashboard/DashProduct.tsx
@@ -1,5 +1,21 @@
+import ItemNotFound from "@/components/ItemNotFound";
+import { Card, CardTitle } from "@/components/ui/card";
+
 function DashProduct() {
-  return <div>DashProduct</div>;
+  return (
+    <div className="flex flex-col h-full w-full gap-5">
+      <section className="flex justify-between items-center">
+        <CardTitle>상품관리</CardTitle>
+      </section>
+      <section className="flex h-full">
+        <Card className="w-full">
+          <ItemNotFound
+            description={`등록된 상품이 없습니다.\n상품을 등록하세요.`}
+          />
+        </Card>
+      </section>
+    </div>
+  );
 }
 
 export default DashProduct;
