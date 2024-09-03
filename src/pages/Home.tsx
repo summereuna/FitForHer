41a0a8ed7cbd0947ef4b3c978c33740d62f40f86
summereuna <summereuna@gmail.com>
2024-09-04 05:14:23 +0900

    [FIX]:셀러 로그인시 대시보드로 리다이렉트 되도록 해결 #43

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const { userRole, isSessionLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isSessionLoading && userRole === "seller") {
+      navigate("/dashboard");
+    }
+  }, [isSessionLoading, userRole, navigate]);
+
   return <div>Home</div>;
 }
